a1a49b27bd1879fd30a007922296e8f22b2d9c56
Victoria Kee <victoriakee95@gmail.com>
2020-06-29 10:23:18 -0500

    Write randomTime

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
