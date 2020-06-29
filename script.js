ac088aa88f3982413b17a3cb9d66bb159a94f31c
Victoria Kee <victoriakee95@gmail.com>
2020-06-29 10:28:15 -0500

    Write peep function

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ function randomHole(holes) {
   lastHole = hole;
   return hole;
 }
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if(!timeUp) peep()
+  }, time);
+}
